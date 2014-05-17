fbab89e7a75e7127002d2ceaaca679c3a205b1c9
Gabriel Barzola <gbarzola@virtualmindsoftware.com>
2014-05-17 02:07:53 -0300

    Console plugin

--- www/js/index.js
+++ www/js/index.js
@@ -34,21 +34,21 @@ var app = {
     // function, we must explicity call 'app.receivedEvent(...);'
     onDeviceReady: function() {
         var xhr = new XMLHttpRequest();
- xhr.open('GET', 'https://api.github.com/legacy/repos/search/javascript', true);
-  // Response handlers.
-  xhr.onload = function () {
-     var repos = JSON.parse(xhr.response), i, reposHTML = "";
-     for (i = 0; i < repos.repositories.length; i++) {
-       reposHTML += "<p><a href='https://github.com/" + repos.repositories[i].username + "/" + repos.repositories[i].name + "'>" + repos.repositories[i].name + "</a><br>" + repos.repositories[i].description + "</p>";
-     }
-     document.getElementById("allRepos").innerHTML = reposHTML;
-  };
+         xhr.open('GET', 'https://api.github.com/legacy/repos/search/javascript', true);
+          // Response handlers.
+          xhr.onload = function () {
+             var repos = JSON.parse(xhr.response), i, reposHTML = "";
+             for (i = 0; i < repos.repositories.length; i++) {
+               reposHTML += "<p><a href='https://github.com/" + repos.repositories[i].username + "/" + repos.repositories[i].name + "'>" + repos.repositories[i].name + "</a><br>" + repos.repositories[i].description + "</p>";
+             }
+             document.getElementById("allRepos").innerHTML = reposHTML;
+          };
 
-  xhr.onerror = function () {
-     alert('error making the request.');
-  };
+          xhr.onerror = function () {
+             alert('error making the request.');
+          };
 
-xhr.send();
+        xhr.send();
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
